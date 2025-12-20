719e7ff857442010c0ff45611ab2b0088901638d
danil <macrulezru@gmail.com>
2025-12-21 00:50:33 +0300

    Fix reactive tabs titles

--- src/view/ui/ui-tabs/ui-tabs.vue
+++ src/view/ui/ui-tabs/ui-tabs.vue
@@ -1,46 +1,56 @@
 <script setup lang="ts">
-  import '@/view/ui/ui-tabs/ui-tabs.scss'
+import '@/view/ui/ui-tabs/ui-tabs.scss'
+import { ref, provide, type Ref, onMounted } from 'vue'
 
-  import { ref, provide, type Ref, onMounted } from 'vue'
+interface Tab {
+  title: string
+  id?: string
+  hash: string
+}
 
-  interface Tab {
-    title: string
-    id?: string
-    hash: string
+const emit = defineEmits<{
+  mounted: []
+}>()
+
+const activeTabHash = ref('')
+const tabs: Ref<Array<Tab>> = ref([])
+
+const setActiveTab = (tab: Tab) => {
+  activeTabHash.value = tab.hash
+}
+
+const preselectTab = (id: string) => {
+  const foundTab = tabs.value.find(tab => tab.id === id)
+  if (foundTab) {
+    setActiveTab(foundTab)
   }
+}
 
-  const emit = defineEmits<{
-    mounted: []
-  }>()
-
-  const activeTabHash = ref('')
-  const tabs: Ref<Array<Tab>> = ref([])
-
-  const setActiveTab = (tab: Tab) => {
+provide('tshTabsAddTab', (tab: Tab) => {
+  const count = tabs.value.push(tab)
+  if (count === 1) {
     activeTabHash.value = tab.hash
   }
+})
 
-  const preselectTab = (id: string) => {
-    const foundTab = tabs.value.find(tab => tab.id === id)
-    if (foundTab) {
-      setActiveTab(foundTab)
+provide('tshTabsUpdateTab', (oldHash: string, updatedTab: Tab) => {
+  const index = tabs.value.findIndex(tab => tab.hash === oldHash)
+  if (index !== -1) {
+    tabs.value[index] = updatedTab
+
+    if (activeTabHash.value === oldHash) {
+      activeTabHash.value = updatedTab.hash
     }
   }
+})
 
-  provide('tshTabsAddTab', (tab: Tab) => {
-    const count = tabs.value.push(tab)
-    if (count === 1) {
-      activeTabHash.value = tab.hash
-    }
-  })
+provide('tshTabsActiveHash', activeTabHash)
 
-  provide('tshTabsActiveHash', activeTabHash)
+defineExpose({ preselectTab })
 
-  defineExpose({ preselectTab })
-
-  onMounted(() => {
-    emit('mounted')
-  })
+onMounted(() => {
+  emit('mounted')
+})
 </script>
 
 <template>
@@ -49,7 +59,7 @@
       <div class="ui-tabs__navbar">
         <div
           v-for="tab in tabs"
-          :key="tab.title"
+          :key="tab.hash"
           class="ui-tabs__navbar-item"
           :class="{
             'ui-tabs__navbar-item_active': tab.hash === activeTabHash,
