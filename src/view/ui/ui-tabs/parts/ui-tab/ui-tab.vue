6a340de2c1aa98685b19f3096d60322ad9e4d2fd
danil <macrulezru@gmail.com>
2025-12-22 13:34:38 +0300

    API

--- src/view/ui/ui-tabs/parts/ui-tab/ui-tab.vue
+++ src/view/ui/ui-tabs/parts/ui-tab/ui-tab.vue
@@ -1,60 +1,60 @@
 <script setup lang="ts">
-import { onBeforeMount, watch, inject, ref, type Ref, computed } from 'vue'
-import '@/view/ui/ui-tabs/parts/ui-tab/ui-tab.scss'
+  import { onBeforeMount, watch, inject, ref, type Ref, computed } from 'vue'
+  import '@/view/ui/ui-tabs/parts/ui-tab/ui-tab.scss'
 
-interface Props {
-  title: string
-  id?: string
-}
-
-const props = defineProps<Props>()
-
-const tshTabsAddTab: any = inject('tshTabsAddTab')
-const tshTabsUpdateTab: any = inject('tshTabsUpdateTab')
-const tshTabsActiveHash = inject('tshTabsActiveHash')
-
-const hash = computed(() => {
-  return `#${props.title.toLowerCase().replace(/ /g, '-')}`
-})
-
-const isActive = ref(false)
-
-const initialHash = ref('')
-
-watch(
-  () => {
-    return (tshTabsActiveHash as Ref).value
-  },
-  () => {
-    isActive.value = (tshTabsActiveHash as Ref).value === hash.value
-  },
-)
-
-onBeforeMount(() => {
-  initialHash.value = hash.value
-
-  tshTabsAddTab({
-    title: props.title,
-    id: props.id,
-    hash: hash.value,
-  })
-})
-
-watch(
-  () => props.title,
-  (newTitle, oldTitle) => {
-    const oldHash = `#${oldTitle.toLowerCase().replace(/ /g, '-')}`
-    const newHash = hash.value
-
-    if (tshTabsUpdateTab) {
-      tshTabsUpdateTab(oldHash, {
-        title: newTitle,
-        id: props.id,
-        hash: newHash,
-      })
-    }
+  interface Props {
+    title: string
+    id?: string
   }
-)
+
+  const props = defineProps<Props>()
+
+  const tshTabsAddTab: any = inject('tshTabsAddTab')
+  const tshTabsUpdateTab: any = inject('tshTabsUpdateTab')
+  const tshTabsActiveHash = inject('tshTabsActiveHash')
+
+  const hash = computed(() => {
+    return `#${props.title.toLowerCase().replace(/ /g, '-')}`
+  })
+
+  const isActive = ref(false)
+
+  const initialHash = ref('')
+
+  watch(
+    () => {
+      return (tshTabsActiveHash as Ref).value
+    },
+    () => {
+      isActive.value = (tshTabsActiveHash as Ref).value === hash.value
+    },
+  )
+
+  onBeforeMount(() => {
+    initialHash.value = hash.value
+
+    tshTabsAddTab({
+      title: props.title,
+      id: props.id,
+      hash: hash.value,
+    })
+  })
+
+  watch(
+    () => props.title,
+    (newTitle, oldTitle) => {
+      const oldHash = `#${oldTitle.toLowerCase().replace(/ /g, '-')}`
+      const newHash = hash.value
+
+      if (tshTabsUpdateTab) {
+        tshTabsUpdateTab(oldHash, {
+          title: newTitle,
+          id: props.id,
+          hash: newHash,
+        })
+      }
+    },
+  )
 </script>
 
 <template>
