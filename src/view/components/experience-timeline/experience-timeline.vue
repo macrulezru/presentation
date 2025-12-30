a0ca73f553fa86476c9a0b32f718e752748cecfc
macrulezru <macrulezru@gmail.com>
2025-12-30 22:43:11 +0300

    feat: add company logos and experience timeline component
    - Added SVG logo for Tais and PNG logos for TNT and UtiNet.
    - Created experience-items.ts to manage experience items with associated logos and details.
    - Updated experience-timeline.scss for improved layout and styling of experience items.
    - Refactored experience-timeline.vue to utilize the new experience items and enhance responsiveness.
    - Introduced new font weight variable for typography consistency.

--- src/view/components/experience-timeline/experience-timeline.vue
+++ src/view/components/experience-timeline/experience-timeline.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
+  import '@/view/components/experience-timeline/experience-timeline.scss';
+
   import { ref, computed } from 'vue';
 
+  import { useExperienceItems } from './experience-items';
+
   import { useI18n } from '@/view/composables/use-i18n';
+  import { useResponsive } from '@/view/composables/use-responsive';
   import Button from '@/view/ui/ui-button/ui-button.vue';
   import LinkArrow from '@/view/ui/ui-link-arrow/ui-link-arrow.vue';
 
-  import '@/view/components/experience-timeline/experience-timeline.scss';
-
-  const { t, tm } = useI18n();
-
-  const experienceItems = computed(() => (tm('experience.items') as any[]) || []);
+  const { t } = useI18n();
+  const { isMobile, isTablet, isDesktop } = useResponsive();
 
   const showAll = ref(false);
 
   // Показываем только первые 4 компании, если не нажата кнопка "Показать все"
+
+  const experienceItems = useExperienceItems(t);
   const displayedItems = computed(() => {
     return showAll.value ? experienceItems.value : experienceItems.value.slice(0, 4);
   });
@@ -29,43 +33,47 @@
 
       <div class="experience__timeline">
         <div
-          v-for="(item, index) in displayedItems"
-          :key="index"
+          v-for="item in displayedItems"
+          :key="item.id"
           class="experience__item"
+          :class="{ 'experience__item_no-logo': !item.logo }"
         >
-          <!-- Содержимое карточки -->
+          <div
+            v-if="((isMobile || isTablet) && item.logo) || isDesktop"
+            class="experience__logo-container"
+          >
+            <img
+              v-if="item.logo"
+              class="experience__logo"
+              :src="item.logo"
+              :alt="item.company"
+            />
+          </div>
           <div class="experience__card">
-            <!-- Период (только если есть) -->
-            <div v-if="item.period" class="experience__period">
-              {{ item.period }}
-            </div>
-
             <div class="experience__card-header">
-              <div>
-                <h3 class="experience__company">
-                  <template v-if="item.url">
-                    <a
-                      v-if="item.url"
-                      class="experience__link"
-                      :href="item.url"
-                      target="_blank"
-                    >
-                      {{ item.company }}
-                      <LinkArrow />
-                    </a>
-                  </template>
-                  <template v-else>
+              <h3 class="experience__company">
+                <template v-if="item.url">
+                  <a
+                    v-if="item.url"
+                    class="experience__link"
+                    :href="item.url"
+                    target="_blank"
+                  >
                     {{ item.company }}
-                  </template>
-                </h3>
+                    <LinkArrow />
+                  </a>
+                </template>
+                <template v-else>
+                  {{ item.company }}
+                </template>
+              </h3>
+              <div>
+                <div v-if="item.period" class="experience__period">
+                  {{ item.period }}
+                </div>
+                <div class="experience__position">{{ item.position }}</div>
               </div>
-              <!-- Длительность (только если есть) -->
-              <span v-if="item.duration" class="experience__duration">
-                {{ item.duration }}
-              </span>
             </div>
-            <div class="experience__position">{{ item.position }}</div>
-
             <div class="experience__description">
               {{ item.description }}
             </div>
@@ -73,7 +81,6 @@
         </div>
       </div>
 
-      <!-- Кнопка "Показать все компании" -->
       <div v-if="hasMore" class="experience__button-container">
         <Button fullWidth :text="t('experience.showAllButton')" @click="showAll = true" />
       </div>
