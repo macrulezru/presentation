62e1c1875a3a78f568fd9af71ef9e77e7f27d1dc
macrulezru <macrulezru@gmail.com>
2026-01-25 23:46:34 +0300

    feat: add Toolz component with localization and styling

--- src/view/components/examples/examples.vue
+++ src/view/components/examples/examples.vue
@@ -16,6 +16,7 @@
   import musicUrl from '@/view/assets/music/background-music.mp3';
   import FeatureItem from '@/view/components/examples/parts/feature-item/feature-item.vue';
   import NpmPackages from '@/view/components/examples/parts/npm-packages/npm-packages.vue';
+  import Toolz from '@/view/components/examples/parts/toolz/toolz.vue';
   import { useFeatures } from '@/view/composables/use-features.ts';
   import { useI18n } from '@/view/composables/use-i18n.ts';
   import { useScrollRouting } from '@/view/composables/use-scroll-routing.ts';
@@ -315,6 +316,9 @@
     </div>
     <div class="examples__npm">
       <NpmPackages />
+    </div>
+    <div class="examples__toolz">
+      <Toolz />
     </div>
   </div>
 </template>
