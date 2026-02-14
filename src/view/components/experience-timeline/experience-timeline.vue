d22b4ceed81d9c48e2b319e0a2d8f23ab5f829e6
macrulezru <macrulezru@gmail.com>
2026-02-14 12:47:50 +0300

    feat: add loading indicators to experience-timeline and npm-packages components

--- src/view/components/experience-timeline/experience-timeline.vue
+++ src/view/components/experience-timeline/experience-timeline.vue
@@ -7,10 +7,13 @@
   import CompanyItem from './parts/company-item/company-item.vue';
 
   import { useI18n } from '@/view/composables/use-i18n';
+  import UiLoading from '@/view/ui/ui-loading/ui-loading.vue';
 
   const { t } = useI18n();
 
   const experienceItems = useExperienceItems();
+
+  const isLoading = computed(() => experienceItems.loading.value);
 
   const headCompany = computed(() => experienceItems.items.value.slice(0, 2));
 
@@ -21,30 +24,34 @@
   <div class="experience">
     <div class="experience__container">
       <div class="experience__title">{{ t('experience.title') }}</div>
+      <div v-if="isLoading" class="experience__loader">
+        <UiLoading type="circle" progressColor="#d941b0" />
+      </div>
+      <template v-else>
+        <div class="experience__years">
+          <span class="experience__years-text">2025 - 2017</span>
+        </div>
+        <div class="experience__companies-header">
+          <CompanyItem
+            v-for="company in headCompany"
+            :key="company.id"
+            :company="company"
+            isHeader
+          />
+        </div>
 
-      <div class="experience__years">
-        <span class="experience__years-text">2025 - 2017</span>
-      </div>
-      <div class="experience__companies-header">
-        <CompanyItem
-          v-for="company in headCompany"
-          :key="company.id"
-          :company="company"
-          isHeader
-        />
-      </div>
+        <div class="experience__years">
+          <span class="experience__years-text">2017 - 1998</span>
+        </div>
 
-      <div class="experience__years">
-        <span class="experience__years-text">2017 - 1998</span>
-      </div>
-
-      <div class="experience__companies">
-        <CompanyItem
-          v-for="company in overCompany"
-          :key="company.id"
-          :company="company"
-        />
-      </div>
+        <div class="experience__companies">
+          <CompanyItem
+            v-for="company in overCompany"
+            :key="company.id"
+            :company="company"
+          />
+        </div>
+      </template>
     </div>
   </div>
 </template>
