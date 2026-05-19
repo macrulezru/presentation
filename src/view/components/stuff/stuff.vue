06497c80677c9fe5ea7006436c4e1341f98df457
macrulezru <macrulezru@gmail.com>
2026-05-19 13:14:27 +0300

    Add airlines section

--- src/view/components/stuff/stuff.vue
+++ src/view/components/stuff/stuff.vue
@@ -4,6 +4,7 @@
   import type { BlogPostItem } from '@/view/components/stuff/parts/blog/parts/blog-items';
   import type { NpmPackageItem } from '@/view/components/stuff/parts/npm-packages/npm-items';
 
+  import Airlines from '@/view/components/stuff/parts/airlines/airlines.vue';
   import Blog from '@/view/components/stuff/parts/blog/blog.vue';
   import NpmPackages from '@/view/components/stuff/parts/npm-packages/npm-packages.vue';
   import Toolz from '@/view/components/stuff/parts/toolz/toolz.vue';
@@ -19,6 +20,7 @@
     <div class="stuff__container">
       <NpmPackages :ssrItems="ssrNpmItems" />
       <Toolz />
+      <Airlines />
       <Blog :ssrItems="ssrBlogItems" />
     </div>
   </div>
