80e7c08351896eeb80ba6c0b22537b7ab42cc78b
macrulezru <macrulezru@gmail.com>
2026-09-01 14:32:10 +0300

    Image optimization

--- src/view/components/stuff/parts/airlines/airlines.vue
+++ src/view/components/stuff/parts/airlines/airlines.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import './airlines.scss';
 
-  import airlinesImage from '@/view/assets/images/airlines.png';
+  import airlinesImage from '@/view/assets/images/airlines.webp';
   import WorldRoute from '@/view/components/world-route/world-route.vue';
   import { useI18n } from '~/composables/useI18n';
 
